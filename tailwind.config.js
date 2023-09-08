c79e8bf56cd942aa7a8bcea5c51910184155d607
“Akbarwp” <akbarwp002@gmail.com>
2023-09-08 18:21:06 +0700

    Add Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
     content: ["./src/**/*.{html,js}", "index.html"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sacramento: ["Sacramento"],
+                workSans: ["Work Sans"],
+            },
+            colors: {
+                pinkPrimary: "#F14E95",
+                bgPrimary: "#0A0A0A",
+            },
+            screens: {
+                // laptop: {"max": "992px"},
+                // tablet: {"max": "768px"},
+                // mobile: {"max": "576px"},
+            },
+        },
     },
     plugins: [],
 };
